1fb596995ae234727cf1319dbb6e940fbe436a3c
harsh21169 <harshdepani2509@gmail.com>
2026-02-04 09:18:09 +0530

    3/2/2026/user/login

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,25 +1,27 @@
 const Users = require("../model/user.model");
 const bcrypt = require('bcrypt');
 const sendMail = require("../servicer/nodemailer");
+const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser');
 
 const register = async (req, res) => {
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
         const hashPass = await bcrypt.hash(password, 10)
         const OTP = Math.floor(100000 + Math.random() * 900000)
-        const userExites = await Users.findOne({email})
+        const userExites = await Users.findOne({ email })
         const user = await Users.create({ ...req.body, password: hashPass, OTP: OTP });
 
-        if(userExites){
+        if (userExites) {
             return res.status(400).json({ sucess: false, data: null, Message: "User Already Exites" })
         }
 
         sendMail(email, "Register OTP ", "Your OTP is : " + OTP);
 
-        const userdata = await Users.findOne({email}).select('-password')
+        const userdata = await Users.findOne({ email }).select('-password -OTP')
 
         if (!user) {
-            return res.status(400).json({ sucess: false, data: [], Message: "Users not added" })
+            return res.status(400).json({ sucess: false, data: [], Message: "Users register not complete" })
         }
 
         return res.status(200).json({ sucess: true, data: userdata, Message: "Users register sucessfully." })
@@ -30,8 +32,73 @@ const register = async (req, res) => {
 
 }
 
+const is_verify = async (req,res) => {
+    try {
+        const {email, OTP} = req.body;
+        
+        const userverify = await Users.findOne({email: email , OTP : OTP})
 
+        if(!userverify){
+            return res.status(400).json({ sucess: false, data: null, Message: "Invaild Username/Email or OTP" })
+        }
+
+        userverify.is_verify = true;
+
+        userverify.save();
+
+        return res.status(200).json({ sucess: true, data: userverify, Message: "Users Verfivcation Complete." })
+        
+
+    } catch (error) {
+        return res.status(500).json({ sucess: false, data: null, Message: "Internal Server Error :" + error })
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+    const userExites = await Users.findOne({ email })
+
+    if (!userExites) {
+        return res.status(400).json({ sucess: false, data: null, Message: "User not Exites" })
+    }
+
+    const passisMatched = await bcrypt.compare(password, userExites.password);
+    console.log(passisMatched);
+
+    if(!passisMatched){
+        return res.status(400).json({ sucess: false, data: null, Message: "Invaild Password or Username/Email" })
+    }
+
+    const accessToken = jwt.sign({
+        email: userExites.email
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '10m'
+    });
+
+    const refreshToken = jwt.sign({
+        email: userExites.email,
+    }, process.env.REFRESH_TOKEN_SECRET, { 
+        expiresIn: '1d' });
+
+    res.cookie('LoginCookies', refreshToken, {
+        httpOnly: true,
+        sameSite: 'None', secure: true,
+        maxAge: 24 * 60 * 60 * 1000
+    }); 
+
+    // return res.status(200).json({ sucess: true, data: userExites, Message: "Users logined Complete." })
+    return res.json({ accessToken , refreshToken});
+
+    } catch (error) {
+        return res.status(500).json({ sucess: false, data: null, Message: "Internal Server Error :" + error })
+    }
+
+}
 
 module.exports = {
-   register
+    register,
+    is_verify,
+    login
 }
--- src/routers/api/v2/user.router.js
+++ src/routers/api/v2/user.router.js
@@ -3,5 +3,7 @@ const { userscontroller } = require('../../../controller');
 const routers = express.Router()
 
 routers.post("/register", userscontroller.register)
+routers.post("/login", userscontroller.login)
+routers.post("/is_verify", userscontroller.is_verify)
 
 module.exports = routers;
