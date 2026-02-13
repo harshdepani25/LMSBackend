b919624c56466104dcfacd02080e42abfd167cd6
harsh21169 <harshdepani2509@gmail.com>
2026-02-13 20:19:56 +0530

    13/2/2026/scoketio

--- src/servicer/socketIO.js
+++ src/servicer/socketIO.js
@@ -1,20 +1,32 @@
+const { log } = require('node:console');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
 
 const createSocketIO = () => {
     try {
         const io = new Server({
-            cors : {
-                origin : "http://localhost:5174"
+            cors: {
+                origin: "http://localhost:5173"
             }
-        }); 
+        });
 
         io.on('connection', (socket) => {
             socket.emit("welcome", 'Welcome to our chat.')
             console.log('a user connected', socket.id);
+
+            socket.on("userMSG:", ({msg, totheid}) => {
+                console.log("msg: ", msg);
+                console.log("id: ", totheid);
+                
+                io.to(totheid).emit("resevier-msg", msg)
+            });
+
+            socket.on("Group", (group) => {
+                socket.join(group)
+            })
+
         });
-
-
+       
         io.listen(4000)
 
     } catch (error) {
