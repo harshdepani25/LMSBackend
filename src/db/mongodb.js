1b3dd3c408fbffebf17aabf7555f151a38f5e7f7
Harsh Depani <harshdepani2509@gmail.com>
2026-03-23 18:34:38 +0530

    qawsed

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -3,8 +3,8 @@ const { default: mongoose } = require("mongoose");
 const MongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URL)
-            .then(() => console.log("Mongodb connected sugsecfully"))
-            .catch(() => console.log("Error to connect : " + error))    
+            .then(() => console.log("Mongodb connected sugecfully"))
+            .catch((error) => console.log("Error to connect : " + error))    
         } catch (error) {
             console.log(error);
             
